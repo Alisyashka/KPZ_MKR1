45a7443407ede68882c56392cd65562561ed2a98
OleksandrValinkevich <ipz244_vos@student.ztu.edu.ua>
2026-05-19 02:07:46 +0300

    add command

---
+++ ClickCommand.cs
@@ -0,0 +1,29 @@
+﻿namespace Task_5
+{
+    namespace Task_5
+    {
+        public class ClickCommand : ICommand
+        {
+            public void Execute()
+            {
+                Console.WriteLine("Button clicked!");
+            }
+        }
+
+        public class SecondClickCommand : ICommand
+        {
+            public void Execute()
+            {
+                Console.WriteLine("Second click handler!");
+            }
+        }
+
+        public class MouseOverCommand : ICommand
+        {
+            public void Execute()
+            {
+                Console.WriteLine("Mouse over button!");
+            }
+        }
+    }
+}
